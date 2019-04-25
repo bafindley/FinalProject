2f2cada03bdd6d21b1ec473d808ba54a099ebd5e
jentmayfield <44143789+jentmayfield@users.noreply.github.com>
2019-04-25 18:54:31 -0500

    Add files via upload

---
+++ Movie_JavaScript2/app.js
@@ -0,0 +1,72 @@
+// from data.js
+var tableData = data_movie;
+var filteredData = tableData
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(tableData);
+
+// BONUS: Refactor to use Arrow Functions!
+function createTable(){
+  tbody.html('');
+  filteredData.forEach((movieReport) => {
+    var row = tbody.append("tr");
+    Object.entries(movieReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+  
+
+
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+submit.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#title");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    
+    filteredData = tableData.filter(title => title.Title === inputValue);
+     
+    console.log(filteredData); 
+    ;
+    createTable()
+    
+        });       
+    createTable()
+
+
+
+
+      // d3.select("#datetime").on('change');
+    
+      // filteredData.forEach((filter) => {
+      //     var row = tbody.append("tr");
+      //     Object.entries(filter).forEach(([key, value]) => {
+      //       var cell = tbody.append("td");
+      //       cell.text(value);  
+      // createTable(filteredData);
+     
+
+         
+  //   }); 
+  // });
+  //  });   
+
+
+   
+
+  
